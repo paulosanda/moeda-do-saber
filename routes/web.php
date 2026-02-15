6cd1077a8ae244dd4f4c5cd2a5c89256739a400d
Paulo Sanda <paulosanda@gmail.com>
2026-02-15 18:26:59 -0300

    web routes

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,16 @@ Route::get('/', [LandingPageController::class, 'index'])->name('landing');
 /**
  * @return void
  */
-function extracted(): void
-{
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
 
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    require __DIR__ . '/auth.php';
-}
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-extracted();
+require __DIR__ . '/auth.php';
+
