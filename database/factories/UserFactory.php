57e926ec68cd4bbe0c66e206024bf47b160f9805
Paulo Sanda <paulosanda@gmail.com>
2026-02-15 18:21:26 -0300

    user factory modified

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,8 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'active' => true,
+            'admin' => false,
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
